a6136b3968471cf0bb58d9c2c8241f1e2a7f0b5c
hyrexbre <onuresen05@gmail.com>
2021-05-21 11:39:14 +0300

    ZarAt

---
+++ zarat.py
@@ -0,0 +1,14 @@
+ZAR_AT=[
+	"Ata Ata 1 Attın Yazık"
+	"2 mi? eh işte daha şanslı olduğunu düşünüyordum.."
+	"Oooo 3 attın la iyi iyi"
+	"Şansa gel 4 attı.dd"
+	"All 5 Attın işte daha ne istiyon"
+	"BALA GÖTE 6 ATTI"
+	"HAHAAH 1 ATTIN CENABET SENİ"
+	"YA SANSINI SKM 6 ATTIN"
+	"2 ne la AKNVGPNFPASPNFPAS"
+	"3 Yine İyi Diğerlerine Göre"
+	"4 Attın oo kendini aştın"
+	"5 OHH MUTLU OL"
+]
